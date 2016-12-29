e74304651f721bef524cbe92f782a72d8b740b84
curran <curran.kelleher@gmail.com>
2016-12-29 16:00:35 -0500

    Cache compiled templates

--- templateEngine.js
+++ templateEngine.js
@@ -2,25 +2,23 @@ var template = require('lodash.template')
 var fs = require('fs')
 
 var cache = {}
-function getFile (filePath, callback) {
+function getTemplate (filePath, callback) {
   if(cache[filePath]){
     callback(null, cache[filePath])
   } else {
     fs.readFile(filePath, function (err, content) {
       if (err) return callback(err)
-      cache[filePath] = content
-      callback(null, content)
+      cache[filePath] = template(content.toString())
+      callback(null, cache[filePath])
     })
   }
 }
 
 function templateEngine(filePath, options, callback) {
-  getFile(filePath, function (err, content) {
+  getTemplate(filePath, function (err, compiledTemplate) {
     if (err) return callback(err)
-
-    var source = content.toString()
-    var rendered = template(source)(options)
-    return callback(null, rendered)
+    return callback(null, compiledTemplate(options))
   })
 }
+
 module.exports = templateEngine
