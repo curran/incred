8d85ba035985f4ae6e7c971e4a360f566a364397
curran <curran.kelleher@gmail.com>
2016-12-29 15:56:40 -0500

    Cache template files

--- templateEngine.js
+++ templateEngine.js
@@ -1,8 +1,21 @@
 var template = require('lodash.template')
 var fs = require('fs')
 
+var cache = {}
+function getFile (filePath, callback) {
+  if(cache[filePath]){
+    callback(null, cache[filePath])
+  } else {
+    fs.readFile(filePath, function (err, content) {
+      if (err) return callback(err)
+      cache[filePath] = content
+      callback(null, content)
+    })
+  }
+}
+
 function templateEngine(filePath, options, callback) {
-  fs.readFile(filePath, function (err, content) {
+  getFile(filePath, function (err, content) {
     if (err) return callback(err)
 
     var source = content.toString()
